ab862e5d788ea18e0e8aba149534a902525dd573
soonmyeong <smkwon@neonexsoft.com>
2022-01-10 00:24:37 +0900

    인터섹션 타입 시작

---
+++ src/test/java/intersection/IntersectionTypeTest.java
@@ -0,0 +1,14 @@
+package intersection;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class IntersectionTypeTest {
+
+    @DisplayName("")
+    @Test
+    void intersection_test_1(){
+
+    }
+
+}
