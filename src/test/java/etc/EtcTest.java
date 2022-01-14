4db28c93e54ef38600832488d0f4e1cb64e568cc
SoonMyeong_notebook <soon_myeong@naver.com>
2022-01-14 17:29:55 +0900

    병렬 정렬

--- src/test/java/etc/EtcTest.java
+++ src/test/java/etc/EtcTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 @Coffee("아메리카노")
 @Coffee("카레라떼")
@@ -23,4 +25,23 @@ public class EtcTest {
         CoffeeContainer annotation = EtcTest.class.getAnnotation(CoffeeContainer.class);
         Arrays.stream(annotation.value()).forEach(System.out::println);
     }
+
+    @DisplayName("병렬 정렬, 오우?!")
+    @Test
+    void etc_sort() {
+        int size = 1500;
+        int[] numbers = new int[size];
+        Random random = new Random();
+
+        IntStream.range(0,size).forEach(i-> numbers[i] = random.nextInt());
+        long start = System.nanoTime();
+        Arrays.sort(numbers);
+        System.out.println("serial sort : " + (System.nanoTime()-start));
+
+        IntStream.range(0,size).forEach(i-> numbers[i] = random.nextInt());
+        start = System.nanoTime();
+        Arrays.parallelSort(numbers); //병합정렬과 비슷하게 스레드를 더 사용해서 정렬함
+        System.out.println("parallel sort : " + (System.nanoTime()-start));
+    }
+
 }
